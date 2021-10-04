8efa726de9dfa6b0ec05b61b73af3507f4543af9
Ian Hoang <IanHoang@Iansteins-MacBook-Pro.local>
2021-10-03 23:55:19 -0500

    Reformatting data
    Signed-off-by: Ian Hoang <IanHoang@Iansteins-MacBook-Pro.local>

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About Page">
+            <p> This page is about Sarah. </p>
+        </Layout>
+    )
+}
+
+export default AboutPage
